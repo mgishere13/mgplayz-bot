a25ba07341c730d1839d9e3b98b6de64bbbf6bb1
MGPlayzYT <51394649+MGPlayzYT@users.noreply.github.com>
2020-12-29 11:26:40 +0800

    Fix ping command

--- commands/ping.js
+++ commands/ping.js
@@ -5,22 +5,21 @@ module.exports = {
   aliases: ["pong"],
   cooldown: 2,
   disableable: false,
-  execute: message => {
+  execute: async message => {
     const client = message.client;
     const pinging = new MessageEmbed()
       .setDescription(`Pinging...`)
-      .setTimestamp()
-    message.channel.send(pinging).then(async m => {
-      const embed = new MessageEmbed()
-        .setTitle(`:ping_pong: Pong!`)
-        .setColor("RANDOM")
-        .addField(
-          "Message Round Trip",
-          `${Math.abs(m.createdTimestamp - message.createdTimestamp)} ms`
-        )
-        .addField("Discord Heartbeat", `${Math.round(client.ws.ping)} ms`)
-        .setTimestamp()
-     await m.edit(embed);
-    });
+      .setTimestamp();
+    const msg = await message.channel.send(pinging);
+    const embed = new MessageEmbed()
+      .setTitle(`:ping_pong: Pong!`)
+      .setColor("RANDOM")
+      .addField(
+        "Message Round Trip",
+        `${Math.abs(msg.createdTimestamp - message.createdTimestamp)} ms`
+      )
+      .addField("Discord Heartbeat", `${Math.round(client.ws.ping)} ms`)
+      .setTimestamp();
+    msg.edit(embed);
   }
 };
